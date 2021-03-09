5833483b3a19d6b0df37be308cefe40e82e3378d
gron1gh1 <gron1gh1@gmail.com>
2021-03-09 16:01:40 +0900

    Add: market_price check

---
+++ market_price.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/url"
+
+	"github.com/gorilla/websocket"
+)
+
+// [{"ticket":"UNIQUE_TICKET"},{"type":"ticker", "codes":["KRW-EOS"]}]
+
+func main() {
+	u := url.URL{Scheme: "wss", Host: "api.upbit.com", Path: "websocket/v1"}
+	log.Printf("connect %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	oriJson := `[{"ticket":"UNIQUE_TICKET"},{"type":"ticker", "codes":["KRW-EOS"]}]`
+
+	var data []interface{}
+	json.Unmarshal([]byte(oriJson), &data)
+
+	log.Printf("해석 %v\n", data[1].(map[string]interface{})["type"].(string))
+
+	errr := c.WriteJSON(data)
+	if errr != nil {
+		log.Fatal("err", errr)
+	}
+	var data2 map[string]interface{}
+	for {
+
+		err := c.ReadJSON(&data2)
+		if err != nil {
+			log.Println("read:", err)
+			return
+		}
+		log.Printf("recv: %v", data2)
+
+	}
+
+}
